00dfff0a166bfab76082b82379118808bdee8d8a
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2025-12-01 18:21:55 +0200

    add index file

---
+++ main.js
@@ -0,0 +1,25 @@
+//1 не поннял ///
+
+
+
+
+//2//
+
+const teg = document.querySelector('.hello')
+const btn = document.querySelector('button')
+
+btn.addEventListener('click' , function(){
+  if ( teg.style.color !== "red")
+    teg.style.color = "red";
+else (teg.style.color = "")
+})
+
+
+
+
+
+//3 не понял//
+//const img = querySelector("img")
+
+//const random = Math.random() * 4;
+
